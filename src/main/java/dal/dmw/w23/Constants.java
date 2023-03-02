df1d9492b21fab9e36b3d54d4bb1263db5535dcc
Dhanesh Walte <dhaneshwalte@dal.ca>
2023-03-02 16:56:13 -0400

    added log directories

--- src/main/java/dal/dmw/w23/Constants.java
+++ src/main/java/dal/dmw/w23/Constants.java
@@ -4,10 +4,12 @@ import java.util.Calendar;
 
 public class Constants {
     public static final String fileSeparator = ";";
-    public static final String databasePath = "database/";
+    public static final String databaseDirectory = "database";
+    public static final String databasePath = databaseDirectory + "/";
     public static final String metaFileExtension = ".meta";
     public static final String tableFileExtension = ".table";
-    public static final String logFilePath = "Logs/" + 
+    public static final String logsDirectory = "Logs";
+    public static final String logFilePath = logsDirectory + 
                                              Calendar.getInstance().get(Calendar.YEAR) + "-" +
                                              Calendar.getInstance().get(Calendar.MONTH+1) + "-" +
                                              Calendar.getInstance().get(Calendar.DAY_OF_MONTH) + 
